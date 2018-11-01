3f612c9c58f2f8e99cd33f331495c1ce7dda9e01
sendyakbar <msendyakbars@gmail.com>
2018-11-01 11:20:44 +0700

    js racer done with bonus and obstacle

--- js-racer.js
+++ js-racer.js
@@ -1,10 +1,27 @@
 "use strict"
 
-function diceRoll () {
+let input = process.argv
+let participant = input[2]
+let track = input[3]
+let totalPlayer = getData(participant)
 
+function getData(player) {
+  const dict = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  let arrPlayer = []
+  for (let i = 0; i < player; i++) {
+    arrPlayer.push({
+      name: dict[i],
+      position: 0
+    })
+  }
+  return arrPlayer
 }
 
-function sleep (milliseconds) {
+function diceRoll() {
+  return Math.ceil(Math.random() * 6)
+}
+
+function sleep(milliseconds) {
   var start = new Date().getTime();
   for (var i = 0; i < 1e7; i++) {
     if ((new Date().getTime() - start) > milliseconds) {
@@ -13,28 +30,64 @@ function sleep (milliseconds) {
   }
 }
 
-function printBoard () {
+function printBoard() {
+  for (let i = 0; i < totalPlayer.length; i++) {
+    printLine(totalPlayer[i].name, totalPlayer[i].position)
+  }
+  console.log('> : 5 steps forward')
+  console.log('* : 5 steps backward')
+}
+
+function printLine(player, pos) {
+  let arr = []
+  for (let i = 0; i < track; i++) {
+    if (i === pos) {
+      arr.push(player)
+    }
+    else if (i === Math.floor(track / 4)) {
+      arr.push('>')
+    }
+    else if (i === track - 5) {
+      arr.push('*')
+    }
+    else {
+      arr.push(' ')
+    }
+  }
+  console.log(arr.join('|'))
 
 }
 
-function printLine (player, pos) {
-
+function advance(player) {
+  let limit = 'start'
+  while (limit === 'start') {
+    for (let i = 0; i < player; i++) {
+      totalPlayer[i].position += diceRoll()
+      if (totalPlayer[i].position === Math.floor(track / 4)) totalPlayer[i].position += 5
+      else if (totalPlayer[i].position === track - 5) totalPlayer[i].position -= 5
+      else if (totalPlayer[i].position >= track - 1) {
+        totalPlayer[i].position = track - 1
+        limit = 'stop'
+        clearScreen()
+        printBoard()
+        winner(i)
+        break
+      }
+      sleep(300)
+      clearScreen()
+      printBoard()
+    }
+  }
 }
 
-function advance (player) {
-
+function winner(index) {
+  console.log(`Congratulations! player ${totalPlayer[index].name.toUpperCase()} is the winner`)
 }
 
-function finished () {
-
-}
-
-function winner () {
-
-}
-
-function clearScreen () {
+function clearScreen() {
   // Un-comment this line if you have trouble with console.clear();
   // return process.stdout.write('\033c');
   console.clear();
 }
+
+advance(participant)
