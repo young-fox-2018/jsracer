57e56880c266d196b4f9305eb8476fe1957a78d1
Ribka Haryuni <ribka.blue@gmail.com>
2018-11-01 22:05:27 +0700

    js racer file

--- js-racer.js
+++ js-racer.js
@@ -1,7 +1,7 @@
 "use strict"
 
 function diceRoll () {
-
+  return Math.ceil(Math.random() * 6)
 }
 
 function sleep (milliseconds) {
@@ -14,11 +14,54 @@ function sleep (milliseconds) {
 }
 
 function printBoard () {
-
+  const argv = process.argv
+  let players = Number(argv[2])
+  let track = Number(argv[3])
+  if (players < 2 || players > 26 || track < 15) console.log("Players min 2, max 26 & track minimal 15")
+  else printLine(players, track)
 }
 
-function printLine (player, pos) {
+function printLine (players, pos) {
+  const dictionary = "abcdefghijklmnopqrstuvwxyz"
+  let playerPosition = []
 
+  for(let possibleStep = 0; possibleStep < pos; possibleStep++){
+    let result = []
+    for(let i = 0; i < players; i++){  
+      let random = diceRoll()    
+      if(possibleStep === 0){
+        let obj = {}
+        obj.name = i
+        obj.position = 0
+
+        playerPosition.push(obj)
+      }else{
+        playerPosition[i].position += random
+        if(playerPosition[i].position >= pos) playerPosition[i].position = pos-1
+      }     
+      
+      let string = ""
+      for(let j = 0; j < pos; j++){
+        string += "|"
+        if(j === playerPosition[i].position){
+          string += dictionary[i]
+        }else{
+          string += " "
+        }
+      }
+      result.push(string)
+    }
+    clearScreen()
+    console.log(result)
+    let winner = ""
+    playerPosition.forEach(item =>{
+      if(item.position === pos-1){
+        winner += `Winner is ${dictionary[item.name]} !`
+      }
+    })
+    if (winner !== "") return winner
+    sleep(2000)    
+  }
 }
 
 function advance (player) {
@@ -38,3 +81,5 @@ function clearScreen () {
   // return process.stdout.write('\033c');
   console.clear();
 }
+
+printBoard()
